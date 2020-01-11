45be923a6f1e13c5d9d4e53e83ac16d5b21f8b78
Lucas Wilber <lucasjwilber@gmail.com>
2020-01-10 21:26:38 -0800

    cleaned up some code, added a rotation method

--- src/main/java/bitmap/transformer/BitMap.java
+++ src/main/java/bitmap/transformer/BitMap.java
@@ -5,42 +5,61 @@ import java.awt.image.BufferedImage;
 import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
+
+import static java.awt.image.BufferedImage.TYPE_INT_ARGB;
+
 public class BitMap {
 
-    public static BufferedImage bitMapReader(String path) throws IOException {
-        File bmpFile = new File(path);
-        BufferedImage bmpImage = ImageIO.read(bmpFile);
-        return bmpImage;
+    public static BufferedImage createBufferedImageFromBMP(String path) throws IOException {
+        File bufferedImageFile = new File(path);
+        //turn the given .bmp into a BI file
+        return ImageIO.read(bufferedImageFile);
     }
 
+    public static void saveBufferedImageAsBMP( final BufferedImage image, final String path ){
+        try {
+            ImageIO.write(image, "bmp", new File(path));
+        } catch ( IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-    public static BufferedImage changeColor(String path) throws IOException {
-        final BufferedImage newImage = bitMapReader(path);
+    //transforms:
+    public static BufferedImage flipBlackAndWhite(String path) throws IOException {
+        final BufferedImage newImage = createBufferedImageFromBMP(path);
 
-        for (int x = 0; x < 100; x++){
-            for (int y = 0; y < 100; y++){
+        for (int x = 0; x < newImage.getWidth(); x++) {
+            for (int y = 0; y < newImage.getHeight(); y++) {
                 Color thisPixelColor = new Color(newImage.getRGB(x, y));
-                if (thisPixelColor.getRed() == 255 &&
-                        thisPixelColor.getGreen() == 255 &&
-                        thisPixelColor.getBlue() == 255) {
+                if (thisPixelColor.getRed() >= 245 &&
+                        thisPixelColor.getGreen() >= 245 &&
+                        thisPixelColor.getBlue() >= 245) {
                     newImage.setRGB(x, y, 0);
+                } else if (thisPixelColor.getRed() <= 15 &&
+                        thisPixelColor.getGreen() <= 15 &&
+                        thisPixelColor.getBlue() <= 15) {
+                    newImage.setRGB(x, y, 255);
                 }
             }
         }
         return newImage;
     }
 
+    //this may only work with square images, haven't tested.
+    public static BufferedImage rotateClockwise(String path) throws IOException, ArrayIndexOutOfBoundsException {
+        final BufferedImage oldImage = createBufferedImageFromBMP(path);
+        BufferedImage newImage = createBufferedImageFromBMP(path);
 
-    public static void saveFile( final BufferedImage bi, final String path ){
-        try {
-            RenderedImage rendImage = bi;
-            ImageIO.write(rendImage, "bmp", new File(path));
-        } catch ( IOException e) {
-            e.printStackTrace();
+        for (int x = 0; x < oldImage.getWidth(); x++) {
+            for (int y = 0; y < oldImage.getHeight(); y++) {
+                int thisPixelColor = oldImage.getRGB(x, y);
+                newImage.setRGB(y, oldImage.getHeight() - 1 - x, thisPixelColor);
+            }
         }
+        return newImage;
     }
 
-    
+
     public static void main(String[] args) {
         System.out.println("hi");
     }
